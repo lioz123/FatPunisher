71d0c7d731da979d1192d19833704f9630b10f86
lioz123 <dayanlioz0@gmail.com>
2020-12-27 17:46:24 +0200

    fixed gitignore

---
+++ App.tsx
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+
+  View,
+
+} from 'react-native';
+
+import {Appbar} from 'react-native-paper';
+const App = () => {
+  return (
+    <View>
+      <Appbar.Header>
+        <Appbar.Content title="sdfa"/>
+      </Appbar.Header>
+
+    </View>
+  );
+};
+
+
+export default App;
